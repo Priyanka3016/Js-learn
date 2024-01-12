c27973b557f23652842b9cdfec5c4198f5fda43b
Priyanka3016 <113182500+Priyanka3016@users.noreply.github.com>
2024-01-12 18:00:52 +0530

    Create 02_comparision.js

---
+++ 02_comparision.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+// comparision operator like <,>,==,<=,>=,!=
+
+//js give pridictable result
+console.log("2">1);//true
+console.log("2"<1);//false
+
+console.log(null>0);
+console.log(null==0);
+//IMP
+//Because equality check and comparision operator works differently
+// comparision operator will change , them to number befor comparision
+
+console.log(null>=0);//-->true
+// because null acts as 0 if it act as a number
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);// true
+
+
+// check data type also
+console.log("2" === 2);//false
