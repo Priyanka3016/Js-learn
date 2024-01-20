4ff75ce3b919850c0826deba991504460d19dd29
Priyanka3016 <113182500+Priyanka3016@users.noreply.github.com>
2024-01-21 01:17:30 +0530

    About control logics

---
+++ 11controllogic.js
@@ -0,0 +1,34 @@
+// -->if ,else if, else like c++
+
+// <, >, <=, >=, ==, !=, ===, !==,||,&&
+
+
+//true
+ if (2 =='2' ) {
+    console.log("Execute");
+ }
+ //one line scope
+ if(true)console.log('one line scope');
+
+//switch
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+//----------->false and true
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+//empty array,empty object and function
+
