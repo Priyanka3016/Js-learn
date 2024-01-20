c4dd17021fb1fb59e1dc6b83bfbf587a0d1e1618
Priyanka3016 <113182500+Priyanka3016@users.noreply.github.com>
2024-01-21 00:30:47 +0530

    About scope

---
+++ 09scope.js
@@ -0,0 +1,6 @@
+//curly braces are used for object declation also but there they dont act as scope instead they act as object declaration
+
+//var is globally scoped
+//wheras let and const are block scoped
+
+//closure in nessted funct
